d678a05eae5c130898d29a42269dc896562e00b7
yzlim <yzlim23@gmail.com>
2021-06-15 22:45:49 +0800

    150621

--- src/Count.js
+++ src/Count.js
@@ -7,17 +7,17 @@ const {articles} = data;
 let wordmap = new Map()
 //{{title,url},{title,url}...}
 articles.map((article) => {
-    const {title,url} = article;
-    wordpos.getNouns(title).map((keyword)=>{
-        const word=keyword.toLowerCase();
+    const {title, url} = article;
+    wordpos.getNouns(title, function(result) {
+        result.map((keyword)=>{
+        const word = keyword.toLowerCase();
         if (!wordmap.has(word)) {
-            wordmap.set(word,{count:1,urls:[url],keyword:word});
+            wordmap.set(word, {count:1, urls:[url], keyword:word});
         } else {
             const prev = wordmap.get(word);
-            wordmap.set(word,{count:prev.count+1,urls:[url,...prev.urls],keyword:word});
-        }
-        return 0;
+            wordmap.set(word, {count:prev.count+1, urls:[url, ...prev.urls], keyword:word});
+        }})
     })
-    return 0;
 })
-module.exports={wordmap};
+
+module.exports = {wordmap};
