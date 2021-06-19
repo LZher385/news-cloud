57071fd7f5464ae4a89e2c7b1bd88f7a4f032f25
yzlim <yzlim23@gmail.com>
2021-06-19 19:10:42 +0800

    parsing function now returns array sorted according to word count

--- src/Count.js
+++ src/Count.js
@@ -1,59 +1,42 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(''); // key in ur fackin api
 var WordPOS = require('wordpos'),
 wordpos = new WordPOS();
 const data = require('../top-headlines.json')
 
-
-const {articles} = data;
 let re = /( - [A-Z])|( \| [A-Z])|( - [0-9])|( \| [0-9])/g
 let wordmap = new Map()
-WordPOS.stopwords.push('honduras');
-console.log('GSGSGGSFDFSFDSFSFDFSDF');
-console.log(WordPOS.stopwords[WordPOS.stopwords.length - 1]);
-//{{title,url},{title,url}...}
-articles.map((article) => {
-    const {url, title: originalTitle} = article;
-    let {title} = article;
-    const indMatch = title.search(re);
-    title = indMatch != -1 ? title.substring(0, indMatch) : title;
-    wordpos.getNouns(title, function(result) {
-        result.map((keyword)=>{
-        const word = keyword.toLowerCase();
-        console.log(title);
-        console.log(word);
-        console.log('--------');
-        if (!wordmap.has(word)) {
-            wordmap.set(word, {count:1, urls:[url], keyword:word});
-        } else {
-            const prev = wordmap.get(word);
-            wordmap.set(word, {count:prev.count+1, urls:[url, ...prev.urls], keyword:word});
-        }})
+let countArr = [];
+newsapi.v2.topHeadlines({
+    category: 'general',
+    language: 'en',
+    pageSize: 50
+}).then(response => {
+    const {articles} = response;
+    Promise.all(articles.map(async (article) => {
+        const {url, title: originalTitle} = article;
+        let {title} = article;
+        const indMatch = title.search(re);
+        title = indMatch != -1 ? title.substring(0, indMatch) : title;
+        await wordpos.getNouns(title, function(result) {
+            result.map((keyword)=>{
+            const lowerCaseWord = keyword.toLowerCase();
+            if (!wordmap.has(lowerCaseWord)) {
+                wordmap.set(lowerCaseWord, countArr.length);
+                countArr.push({keyword: lowerCaseWord, count: 1, urls: [url]});
+            } else {
+                const prevIndex = wordmap.get(lowerCaseWord);
+                const prevWordObj = countArr[prevIndex];
+                countArr[prevIndex] = {...prevWordObj, urls:[...prevWordObj.urls, url], count: prevWordObj.count + 1};
+            }})
+        })
+    })).then(x => {
+        countArr.sort((first, second) => first.count - second.count)
+        for (let x = 1; x < 10; x++) {
+            console.log(countArr[countArr.length - x]);
+        }
     })
-    // wordpos.getPOS(title, function(result) {
-    //     const {nouns, verbs, rest} = result;
-    //     const arr = [...rest];
-    //     nouns
-    //     .filter(noun => !verbs.includes(noun))
-    //     .map((keyword)=>{
-    //         arr.push(keyword)
-    //         const word = keyword.toLowerCase();
-    //         if (!wordmap.has(word)) {
-    //             wordmap.set(word, {count:1, urls:[url], titles:[originalTitle], keyword:word});
-    //         } else {
-    //             const prev = wordmap.get(word);
-    //             wordmap.set(word, {count:prev.count+1, urls:[...prev.urls, url], titles:[...prev.titles, originalTitle], keyword:word});
-    //         }})
-    //         console.log(title);
-    //         console.log('rest');
-    //         console.log(rest);
-    //         console.log('nouns');
-    //         console.log(nouns);
-    //         console.log('final');
-    //         console.log(arr);
-    //         console.log('------------');
-    // })
 })
-// setTimeout(() => {
-//     console.log(wordmap);
-// }, 200);
+// TODO: sort out common stopwords (numbers, months etc)
 
 module.exports = {wordmap};
